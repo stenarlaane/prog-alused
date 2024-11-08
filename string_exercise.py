56cb9ee4a66688e3dd2631302694bc1230f5c50d
Stenar <stenar.laane@voco.ee>
2024-11-08 09:28:17 +0200

    Sõned

---
+++ string_exercise.py
@@ -0,0 +1,14 @@
+# Write your code here
+first_name = "James"
+last_name = "Bond"
+full_name = first_name + " " + last_name
+self_description_sentence = f"My name is {last_name}, {first_name} {last_name}."
+cake = "vahukoormarjadtäidispõhi"
+print("""vahukoor\nmarjad\ntäidis\npõhi""")
+original_string = f"Programming is fun!"
+backwards = original_string[::-1]
+every_other = original_string[::2]
+first_word_reversed = original_string[10::-1]
+
+
+
